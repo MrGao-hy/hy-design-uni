f98b27de58e6b0656f8fadb277579604dcce3c60
gaoxh <2036902768@qq.com>
2025-11-29 16:25:35 +0800

    fix: 修改sticky的props参数

--- src/package/components/hy-sticky/props.ts
+++ src/package/components/hy-sticky/props.ts
@@ -1,12 +1,24 @@
 const stickyProps = {
     /** 吸顶时距离顶部的偏移量（单位：px） */
-    zIndex: { type: [Number], default: 99 },
+    zIndex: {
+        type: [String, Number],
+        default: 99
+    },
     /** 吸顶时的层级 */
-    offsetTop: { type: [Number], default: 99 },
+    offsetTop: {
+        type: [String, Number],
+        default: 0
+    },
     /** 是否启用吸顶功能 */
-    enable: { type: Boolean, default: true },
+    enable: {
+        type: Boolean,
+        default: true
+    },
     /** 在scroll-view内使用时，指定scroll-view的类名（不含点） */
-    scrollSelector: { type: String, default: '' }
+    scrollSelector: {
+        type: String,
+        default: ''
+    }
 }
 
 export default stickyProps
