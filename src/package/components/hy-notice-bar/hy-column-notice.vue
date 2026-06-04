f7ca58a2080df2b81959c38870effafb24e1396d
gxh <2036902768@qq.com>
2026-06-04 13:40:10 +0800

    perf: 提取公共的icon减少代码量

--- src/package/components/hy-notice-bar/hy-column-notice.vue
+++ src/package/components/hy-notice-bar/hy-column-notice.vue
@@ -1,63 +1,32 @@
 <template>
-    <view class="hy-notice" @tap="clickHandler">
-        <slot name="icon">
-            <view class="hy-notice__left-icon" v-if="icon">
-                <hy-icon
-                    :name="icon?.name || icon"
-                    :color="icon?.color || color"
-                    :size="icon?.size || 19"
-                ></hy-icon>
-            </view>
-        </slot>
-        <swiper
-            :disable-touch="disableTouch"
-            :vertical="!step"
-            :circular="true"
-            :interval="duration"
-            :autoplay="true"
-            class="hy-notice__swiper"
-            @change="noticeChange"
+    <swiper
+        :disable-touch="disableTouch"
+        :vertical="!step"
+        :circular="true"
+        :interval="duration"
+        :autoplay="true"
+        class="hy-notice__swiper"
+        @change="noticeChange"
+    >
+        <swiper-item
+            v-for="(item, index) in text"
+            :key="index"
+            class="hy-notice__swiper--item"
+            :style="{ justifyContent: justifyContent }"
         >
-            <swiper-item
-                v-for="(item, index) in text"
-                :key="index"
-                class="hy-notice__swiper--item"
-                :style="{ justifyContent: justifyContent }"
-            >
-                <text class="hy-notice__swiper--item__text" :style="[textStyle]">{{ item }}</text>
-            </swiper-item>
-        </swiper>
-        <view class="hy-notice__right-icon" v-if="['link', 'closable'].includes(mode)">
-            <hy-icon
-                v-if="mode === 'link'"
-                :name="IconConfig.RIGHT"
-                :size="17"
-                :color="color"
-            ></hy-icon>
-            <hy-icon
-                v-if="mode === 'closable'"
-                :name="IconConfig.CLOSE"
-                :size="16"
-                :color="color"
-                @click="close"
-            ></hy-icon>
-        </view>
-    </view>
+            <text class="hy-notice__swiper--item__text" :style="[textStyle]">{{ item }}</text>
+        </swiper-item>
+    </swiper>
 </template>
 
 <script setup lang="ts">
-import { type CSSProperties, toRefs, computed, ref, watch } from 'vue'
+import { type CSSProperties, toRefs, computed, watch } from 'vue'
 import noticeBarProps from './props'
-import { addUnit, error, IconConfig } from '../../libs'
-
-// 组件
-import HyIcon from '../hy-icon/hy-icon.vue'
+import { addUnit, error } from '../../libs'
 
 const props = defineProps(noticeBarProps)
 const { text, fontSize, color } = toRefs(props)
-const emit = defineEmits(['click', 'close'])
-
-const index = ref(0)
+const emit = defineEmits(['change'])
 
 watch(
     () => text.value,
@@ -76,20 +45,7 @@ const textStyle = computed<CSSProperties>(() => {
 })
 
 const noticeChange = (e: any) => {
-    index.value = e.detail.current
-}
-
-/**
- * @description 点击通告栏
- * */
-const clickHandler = (index: number) => {
-    emit('click', index)
-}
-/**
- * @description 点击关闭按钮
- * */
-const close = () => {
-    emit('close')
+    emit('change', e.detail.current)
 }
 </script>
 
