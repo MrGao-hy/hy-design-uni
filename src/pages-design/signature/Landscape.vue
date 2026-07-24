221b0f88b2d0b024e39e7950bcb2346f8340ed93
gxh <2036902768@qq.com>
2026-07-24 19:08:21 +0800

    feat: 新增签名横向页面

---
+++ src/pages-design/signature/Landscape.vue
@@ -0,0 +1,20 @@
+<template>
+    <hy-signature height="100vh" @confirm="onConfirm"></hy-signature>
+</template>
+
+<script lang="ts" setup>
+import type { SignatureResult } from '@/package'
+
+definePage({
+    style: {
+        navigationBarTitleText: '横向签名',
+        pageOrientation: 'landscape'
+    }
+})
+
+const onConfirm = (res: SignatureResult) => {
+    uni.navigateBack()
+}
+</script>
+
+<style scoped lang="scss"></style>
